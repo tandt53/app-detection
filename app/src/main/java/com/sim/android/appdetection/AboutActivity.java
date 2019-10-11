47317192a3b833e39919fe3fdf8968204e158e07
tandt1 <v.tandt1@vinid.net>
2019-10-11 20:36:27 +0700

    ver 27

--- app/src/main/java/com/sim/android/appdetection/AboutActivity.java
+++ app/src/main/java/com/sim/android/appdetection/AboutActivity.java
@@ -12,6 +12,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 public class AboutActivity extends AppCompatActivity {
     Button btnEmail;
     Button btnSubmit;
@@ -38,6 +44,18 @@ public class AboutActivity extends AppCompatActivity {
         btnEmail.setOnClickListener(listner);
         btnSubmit.setOnClickListener(listner);
 
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        //ad unit
+        MobileAds.initialize(getApplicationContext(), getResources().getString(R.string.ad_unit_main));
+        AdView mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
     }
 
